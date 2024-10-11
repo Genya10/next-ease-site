c426778166ac2888ecf7111227fd1e6700648ac5
Jenya <113167825+Genya10@users.noreply.github.com>
2024-10-11 21:38:58 +0300

    create About and add taken result from sercer to page.js(main)

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <h1>About us</h1> 
+    <p>Our company the best company in the world
+    </p>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import Link from "next/link"
 
-export default function Home() {
+async function fetchData(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+  return data
+}
+
+export default async function Home() {
+  const result = await fetchData()
+
   return (
     <div>
-     Main page
+    <h1>Main page</h1>   
+    {result.map(post => (
+      <div key={post.id} className="post">
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+        <Link href={`/post/${post.id}`}>Details</Link>
+      </div>
+    ))}
     </div>
   );
 }
